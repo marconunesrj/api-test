c61f4f182790ea978d750e0dc63a66842008f074
marconunesrj <marconunes.rj@hotmail.com>
2024-02-27 12:26:04 -0300

    Acerto na indentacao do código

--- src/test/java/br/com/xmrtecnologia/task/APITest.java
+++ src/test/java/br/com/xmrtecnologia/task/APITest.java
@@ -29,73 +29,77 @@ public class APITest {
 
     @Test
     public void testPostTest() {
-        RestAssured.given()
+        RestAssured
+            .given()
                 .log().all() // Logar a Requisição
                 // Utilizadas as barras, pois eram aspas duplas dentro de aspas duplas
                 .body(" {\"task\": \"Teste via API\", \"dueDate\": \"2050-04-03\"}")
                 .contentType(ContentType.JSON)
-                .when()
+            .when()
                 .post(TODO)
-                .then()
+            .then()
                 .log().all() // Logar e Resposta
         ;
     }
 
     @Test
     public void deveRetornarTarefasTest() {
-        RestAssured.given()
-                .when()
+        RestAssured
+            .given()
+            .when()
                 .get(TODO)
-                .then()
+            .then()
                 .statusCode(200);
     }
 
     @Test
     public void deveAdicionarTarefaComSucessoTest() {
-        RestAssured.given()
+        RestAssured
+            .given()
                 // Utilizadas as barras, pois eram aspas duplas dentro de aspas duplas
                 .body(" {\"task\": \"Teste via API\", \"dueDate\": \"2050-04-03\"}")
                 .contentType(ContentType.JSON)
-                .when()
+            .when()
                 .post(TODO)
-                .then()
+            .then()
                 .statusCode(201);
-
     }
 
     @Test
     public void naoDeveAdicionarTarefaInvalida() {
-        RestAssured.given()
+        RestAssured
+            .given()
                 // Utilizadas as barras, pois eram aspas duplas dentro de aspas duplas
                 .body(" {\"task\": \"Teste via API\", \"dueDate\": \"2010-04-03\"}")
                 .contentType(ContentType.JSON)
-                .when()
+            .when()
                 .post(TODO)
-                .then()
+            .then()
                 .statusCode(400)
                 .body("message", CoreMatchers.is("Due date must not be in past"));
-
     }
 
     @Test
     public void deveRemoverTarefaComSucessoTest() {
         // Inserir Tarefa
-        Integer id = RestAssured.given()
+        Integer id = RestAssured
+            .given()
                 // Utilizadas as barras, pois eram aspas duplas dentro de aspas duplas
                 .body(" {\"task\": \"Tarefa Teste\", \"dueDate\": \"2050-05-03\"}")
                 .contentType(ContentType.JSON)
-                .when()
+            .when()
                 .post(TODO)
-                .then()
+            .then()
                 .log().all()
                 .statusCode(201)
                 .extract().path("id");
 
         // Remover a Tarefa
-        RestAssured.given()
-                .when()
+        RestAssured
+            .given()
+            .when()
                 .delete("/todo/" + id)
-                .then()
+            .then()
                 .statusCode(204);
     }
 
